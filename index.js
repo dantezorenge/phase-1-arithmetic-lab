d051bdd351e3971a68c400b48d63a6d3594f5803
dantezorenge <orenged524@gmail.com>
2023-03-27 02:23:00 +0300

    new update

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// Multiply num1 and num2 to get 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Calculate the remainder of dividing num3 by num4 to get 4
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+
+let numbers = [10, 20, 5, 15];
+let max = Math.max(...numbers);
+console.log(max); 
